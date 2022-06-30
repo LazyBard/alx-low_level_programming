df2a40936565e05f1ed2358890c9ee6b50167a51
Hussaini Ibrahim <alhussainawunu97@gmail.com>
2022-06-30 14:41:25 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /**
  *  * main - Entry point
- *   *
+ *   * print word to output
  *    * Return: Always 0 (Success)
  *     */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
