d810423580bf8304e0207e07cb57446d4abd3e65
Hussaini Ibrahim <alhussainawunu97@gmail.com>
2022-06-30 17:59:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
-Task 4
-
 #include <stdio.h>
 
 /**
  *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *   * Return: Always 0 (Success)
+ *    */
+
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	 puts("\"Programming is like building a multilingual puzzle");
+	   return (0);
 }
