c2a914b1631b6b1a2dfd869f2a67cac0054e84ab
Hussaini Ibrahim <alhussainawunu97@gmail.com>
2022-07-01 19:21:57 -0700

    tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,18 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-  
- /** 
-  * main - display 0 - 9 
-  * Return: 0 if there is no hikky 
-  */ 
- int main(void) 
- { 
-         int num; 
-  
-         for (num = 0; num < 10; num++) 
-                 putchar(num + '0'); 
-         putchar('\n'); 
-         return (0); 
- }
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - display z - a
+ * Return: 0 if there is no hikky
+ */
+int main(void)
+{
+	char za;
+
+	for (za = 'z'; za >= 'a'; za--)
+	{
+		putchar(za);
+	}
+	putchar('\n');
+	return (0);
+}
