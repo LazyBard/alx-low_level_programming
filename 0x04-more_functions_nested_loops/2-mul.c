a3870965e4e33844a9e213ea7cdcd3981f7f92e4
Hussaini Ibrahim <alhussainawunu97@gmail.com>
2022-07-07 20:22:18 -0700

    multiply integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,11 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * mul - Entry Poin
+ * @a : first digit
+ * @b : second digit
+ * Return:a * b
  */
-int main(void)
+int mul(int a, int b)
 {
-    printf("%d\n", mul(98, 1024));
-    printf("%d\n", mul(-402, 4096));
-    return (0);
+	return (a * b);
 }
