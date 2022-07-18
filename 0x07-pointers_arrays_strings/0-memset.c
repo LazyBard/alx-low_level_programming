cc86111dc8f2158a456ff8b2b6f7d0ce71b1940c
Hussaini Ibrahim <alhussainawunu97@gmail.com>
2022-07-18 16:42:26 -0700

    changes made

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * _memcpy - Entry Point
- * @dest : destination character
- * @src : source pointer
+ * _memset - Entry Point
+ * @s : pointer character
+ * @b : replacing character
  * @n : integer size
- * Return:pointer destination
+ * Return:pointer s
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+		s[i] = b;
 	}
-	return (dest);
+	return (s);
 }
