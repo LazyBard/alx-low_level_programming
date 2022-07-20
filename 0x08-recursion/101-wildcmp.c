25c1bccdb5dac3865d0b785b3ec673031fd73e57
Hussaini Ibrahim <alhussainawunu97@gmail.com>
2022-07-20 14:53:13 -0700

    corrections made

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,76 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * primeno - Entry Point
- * @n : character
- * @i : integer
- * Return:int
+ * wildcmp - check the code for Holberton School students.
+ * @s1: string
+ * @s2: string
+ * @a: int
+ * @b: int
+ * @wildUsed: int
+ * Return: Always 0.
  */
-int primeno(int n, int i)
+int checker(char *s1, char *s2, int a, int b, int wildUsed);
+int wildcmp(char *s1, char *s2)
 {
-	if (n % i == 0 && (i == 1 || i == n) && (n != 1 && n > 0))
-	{
-		return (1);
-	}
-	else if (i <= n)
-	{
-		return (primeno(n, i + 1));
-	}
-	else
-	{
-		return (0);
-	}
+return (checker(s1, s2, 0, 0, -1));
 }
 
 /**
- * is_prime_number - Entry Point
- * @n : character
- * Return:int
+ * checkLast - check last char of s2 when s1 ends
+ * @s: string
+ * @i: int
+ * Return: 0 or 1
  */
-
-int is_prime_number(int n)
+int checkLast(char *s, int i)
 {
-	return (primeno(n, 1));
+if (s[i] == '*')
+	return (checkLast(s, i + 1));
+else if (s[i] == '\0')
+	return (1);
+
+return (0);
+
+}
+/**
+ * checker - helper
+ * @s1: string
+ * @s2: string
+ * @a: int
+ * @b: int
+ * @wildUsed: int
+ * Return: Always 0.
+ */
+int checker(char *s1, char *s2, int a, int b, int wildUsed)
+{
+
+if (s1[a] != '\0')
+{
+	if (s2[b] == '\0')
+		return (0);
+	else if (s2[b] == '*')
+	{
+		if (s2[b + 1] == '*')
+			return (checker(s1, s2, a, b + 1, b));
+		else if (s2[b + 1] == s1[a])
+			return (checker(s1, s2, a, b + 1, b));
+		else if (s1[a + 1] != s2[b + 1])
+			return (checker(s1, s2, a + 1, b, b));
+		else if (s1[a + 1] == s2[b + 1])
+			return (checker(s1, s2, a + 1, b + 1, b));
+	}
+	else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
+		return (checker(s1, s2, a + 1, b + 1, wildUsed));
+
+	if (wildUsed == -1)
+		return (0);
+
+	return (checker(s1, s2, a, wildUsed, wildUsed));
+
+}
+if (s2[b] != '\0')
+	return (checkLast(s2, b));
+
+return (1);
+
 }
