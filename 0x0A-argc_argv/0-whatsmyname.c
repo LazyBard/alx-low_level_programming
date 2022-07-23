46843e48a961629006217a4a27ebe62d03ee87c0
Hussaini Ibrahim <alhussainawunu97@gmail.com>
2022-07-22 21:53:33 -0700

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: argument array
+ *
+ * Return: always 0 success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_number(char *s);
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: argument array
+ *
+ * Return: always 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i = 1;
+
+	while (i < argc)
+	{
+		if (!is_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
+
+/**
+ * is_number - checks if a string is a number
+ * @s: pointer to string
+ * Return: 1 if s is a number 0 otherwise
+ */
+int is_number(char *s)
+{
+	while (*s)
+	{
+		if (*s < 48 || *s > 58)
+		{
+			return (0);
+		}
+		s++;
+	}
+	return (1);
+}
